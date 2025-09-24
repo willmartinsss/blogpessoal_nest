8b51c737c6423ae7a4bbb7cb895299194474eb74
willmartinsss <williammartins323@gmail.com>
2025-09-24 15:18:04 -0300

    Crud Att

--- src/postagem/entities/postagem.module.ts
+++ src/postagem/entities/postagem.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Postagem } from "./postagem.entity";
+import { PostagemService } from "../services/postagem.service";
+import { PostagemController } from "../controllers/postagem.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Postagem])],
-  providers: [],
-  controllers: [],
+  providers: [PostagemService],
+  controllers: [PostagemController],
   exports: [],
 })
 
